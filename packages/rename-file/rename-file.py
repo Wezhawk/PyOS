71d4105bb1d2d74e5efe10248b431064325137ea
Wezhawk <everett.wc55@gmail.com>
2025-07-26 08:45:52 -0500

    Updated with better format

---
+++ packages/rename-file/rename-file.py
@@ -0,0 +1,6 @@
+def rename_file(original_file, new_file, log_action=True):
+    contents = read_file(original_file)
+    delete_file(original_file, log_action=False)
+    create_file(new_file, contents, log_action=False)
+    if log_action:
+        log(f"Renamed file {original_file} to {new_file}", process="System")
