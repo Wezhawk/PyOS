0c859e419124f84345f3a65e677a3c9a9a634fa3
Wezhawk <everett.wc55@gmail.com>
2025-09-19 15:54:43 -0500

    Removed log_action parameter from rename-file

--- packages/rename-file/rename-file.py
+++ packages/rename-file/rename-file.py
@@ -1,6 +1,5 @@
-def rename_file(original_file, new_file, log_action=True):
+def rename_file(original_file, new_file):
     contents = read_file(original_file)
     delete_file(original_file, log_action=False)
     create_file(new_file, contents, log_action=False)
-    if log_action:
-        log(f"Renamed file {original_file} to {new_file}", process="System")
+    log(f"Renamed file {original_file} to {new_file}", process="System")
